6ecd3cea8e98e3b9e5055859b5abc834e0ea2578
Dan Rausch <dan.rausch@gmail.com>
2016-11-18 22:27:32 -0500

    Fix typo.

--- src/config.py
+++ src/config.py
@@ -12,12 +12,12 @@ class Config:
     numberOfThreads = 8
 
     # data paths
-    pathToChadwick = ".." + os.sep + "data" + os.sep + "chadwickbureau" + os.sep
+    pathToChadwick = ".." + os.sep + "data" + os.sep + "chadwick-bureau" + os.sep
     pathToBR = ".." + os.sep + "data" + os.sep + "baseball-reference" + os.sep
     pathToFangraphs = ".." + os.sep + "data" + os.sep + "fangraphs" + os.sep
 
     # sql paths
-    pathToChadwickSql = ".." + os.sep + "sql" + os.sep + "chadwickbureau" + os.sep
+    pathToChadwickSql = ".." + os.sep + "sql" + os.sep + "chadwick-bureau" + os.sep
     pathToBRSql = ".." + os.sep + "sql" + os.sep + "baseball-reference" + os.sep
     pathToFangraphsSql = ".." + os.sep + "sql" + os.sep + "fangraphs" + os.sep
 
